5432ae198a306810aa6ef86cf7eb5746ce4c6bdb
ChuVChien <chien59941@gmail.com>
2024-04-20 17:49:10 +0700

    Person

---
+++ Bai_Thuc_Hanh_Java/src/Person/MainClass.java
@@ -0,0 +1,12 @@
+package Person;
+
+public class MainClass {
+    public static void main(String[] args) {
+        person ps = new person();
+        ps.xuatThongTin();
+        ps.nhapThongTin();
+        ps.xuatThongTin();
+        ps.nhapThongTin("Tran Van C", 39, "Thanh pho HCM");
+        ps.xuatThongTin();
+    }
+}
---
+++ Bai_Thuc_Hanh_Java/src/Person/person.java
@@ -0,0 +1,37 @@
+package Person;
+import java.util.Scanner;
+public class person {
+    public String HoTen;
+    public int Tuoi;
+    public String DiaChi;
+
+    public void nhapThongTin()
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Nhap ho ten: ");
+        HoTen = sc.nextLine();
+        System.out.print("Nhap dia chi: ");
+        DiaChi = sc.nextLine();
+        System.out.print("Nhap tuoi: ");
+        Tuoi = sc.nextInt();
+        sc.close();
+    }
+    public void xuatThongTin()
+    {
+        System.out.println("Ho ten: " + HoTen);
+        System.out.println("Dia Chi: " + DiaChi);
+        System.out.println("Tuoi: " + Tuoi);
+    }
+    public person()
+    {
+        HoTen = "Nguyen Van A";
+        DiaChi = "Ha Noi";
+        Tuoi = 18;
+    }
+    public void nhapThongTin(String ten, int tuoi, String diadiem)
+    {
+        HoTen = ten;
+        DiaChi = diadiem;
+        Tuoi = tuoi;
+    }
+}
