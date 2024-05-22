2f828f17a0a665b4891992a44e8517edc43ea5d6
ChuVChien <chien59941@gmail.com>
2024-05-22 16:13:49 +0700

    CVC178_Cau2

--- Bai Kiem Tra/src/CVC178Person.java
+++ Bai Kiem Tra/src/CVC178Person.java
@@ -1,5 +1,21 @@
+import java.util.Scanner;
+
 public class CVC178Person {
     public String hoten;
     public int ma;
     public float diem;
+    public void nhapthongtin()
+    {
+        Scanner sc= new Scanner(System.in);
+        System.out.println("Nhap hoten: ");
+        hoten = sc.nextLine();
+        System.out.println("Nhap ma:");
+        ma =sc.nextInt();
+        System.out.println("Nhap diem: ");
+        diem = sc.nextFloat();
+    }
+    public void inthongtin()
+    {
+        System.out.println("Ho ten: " + hoten + " Ma " + ma + " Diem " + diem );
+    }
 }
