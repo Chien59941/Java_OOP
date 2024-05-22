0e7ac5d9f78d8e1746522109f36e25429eadb8cb
ChuVChien <chien59941@gmail.com>
2024-05-22 16:21:09 +0700

    CVC178_Cau4

---
+++ Bai Kiem Tra/src/CVC178Student.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class CVC178Student extends CVC178Person {
+    public float diemmongmuon;
+    @Override
+    public void nhapthongtin()
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Nhap diem mong muon dat dc: ");
+        diemmongmuon =sc.nextFloat();
+    }
+    public void inthongtin()
+    {
+        System.out.println("diem mong muon: " + diemmongmuon);
+    }
+
+}
